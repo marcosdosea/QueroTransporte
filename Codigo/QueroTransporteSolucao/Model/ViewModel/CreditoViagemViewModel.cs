318744300ec2910f9e1e0ee0ca3f6a40ec3b9fd2
igorb22 <igorb22@live.com>
2019-08-13 18:35:11 -0300

    Adicionando tela de comprar creditos

---
+++ Codigo/QueroTransporteSolucao/Model/ViewModel/CreditoViagemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.ViewModel
+{
+    public class CreditoViagemViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public double Valor { get; set; }
+
+        public CreditoViagemViewModel(int id, string titulo,double valor) {
+            Id = id;
+            Titulo = titulo;
+            Valor = valor;
+        }
+    }
+}
---
+++ Codigo/QueroTransporteSolucao/QueroTransporteWeb/Controllers/ComprarCreditosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model.ViewModel;
+
+namespace QueroTransporteWeb.Controllers
+{
+    public class ComprarCreditosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cv =  new List<CreditoViagemViewModel>();
+
+            cv.Add(new CreditoViagemViewModel(1,"Creditos de 5", 5.00));
+            cv.Add(new CreditoViagemViewModel(2, "Creditos de 10", 10.00));
+            cv.Add(new CreditoViagemViewModel(3, "Creditos de 15", 15.00));
+
+            
+            return View(cv);
+        }
+    }
+}
