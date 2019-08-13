f4130d580f2be8a381b5e8379dfd5f44a2890092
João Alves <joaoalves.cs@gmail.com>
2019-08-13 14:46:28 -0300

    Classe GerenciadorConsumivelVeicular.cs criada.

---
+++ Codigo/QueroTransporteSolucao/Business/GerenciadorConsumivelVeicular.cs
@@ -0,0 +1,45 @@
+﻿using Business;
+using Persistence;
+using QueroTransporte.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Business
+{
+    public class GerenciadorConsumivelVeicular
+    {
+        private readonly BD_QUERO_TRANSPORTEContext _context;
+        public GerenciadorConsumivelVeicular(BD_QUERO_TRANSPORTEContext context)
+        {
+            this._context = context;
+        }
+        
+        /// <summary>
+        /// Insere consumível veicular na base de dados
+        /// </summary>
+        /// <param name="consumivelveicularModel"> </param>
+        /// <returns> </returns>
+        public bool Inserir(ConsumivelVeicularModel objeto)
+        {
+            ConsumivelVeicular _consumivel = new ConsumivelVeicular();
+            Atribuir(objeto, _consumivel);
+
+            _context.Add(_consumivel);
+            return _context.SaveChanges() == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// Altera dados do consumível veicular na base de dados
+        /// </summary>
+        /// <param name="consumivelveicularModel"> </param>
+        /// <returns> </returns>
+        public bool Editar(ConsumivelVeicular objeto)
+        {
+            ConsumivelVeicular _consumivel = new ConsumivelVeicular();
+
+            Atribuir(objeto, _consumivel);
+            _context.Update(_consumivel);
+            return _context.SaveChanges() == 1 ? true : false;
+        }
+    }
+}
