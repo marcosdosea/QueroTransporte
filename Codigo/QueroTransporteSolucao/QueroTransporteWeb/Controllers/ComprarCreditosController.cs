b86613592909f5108919b03cba25c93fdb73c9d0
igorb22 <igorb22@live.com>
2019-08-14 17:52:33 -0300

    adicionando validacoes ao inserir novo saldo

--- Codigo/QueroTransporteSolucao/QueroTransporteWeb/Controllers/ComprarCreditosController.cs
+++ Codigo/QueroTransporteSolucao/QueroTransporteWeb/Controllers/ComprarCreditosController.cs
@@ -2,34 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business;
 using Microsoft.AspNetCore.Mvc;
 using Model.ViewModel;
 using QueroTransporte.Model;
+using QueroTransporte.Negocio;
 
 namespace QueroTransporteWeb.Controllers
 {
     public class ComprarCreditosController : Controller
-    {  
+    {
+
+        private readonly GerenciadorComprarCredito _gerenciadorComprarCredito;
+        private readonly GerenciadorUsuario _gerenciadorUsuario;
+
+        public ComprarCreditosController(GerenciadorUsuario gerenciadorUsuario, GerenciadorComprarCredito gerenciadorComprarCredito)
+        {
+            _gerenciadorComprarCredito = gerenciadorComprarCredito;
+            _gerenciadorUsuario = gerenciadorUsuario;
+        }
+
+        /// <summary>
+        /// Inicia a tela de comprar creditos
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             ViewBag.Creditos = addListaCreditos();
             return View();
         }
 
+        /// <summary>
+        /// Inicia a tela de comprar creditos e pega o objeto com dados da compra
+        /// </summary>
+        /// <param name="cv"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Index(CreditoViagemModel cv)
         {
-            
             ViewBag.Creditos = addListaCreditos();
             if (ModelState.IsValid)
             {
-                 return RedirectToAction(nameof(Index));
+                if (_gerenciadorUsuario.ObterPorId(cv.IdUsuario) != null)
+                {
+                    if (_gerenciadorComprarCredito.Inserir(cv))
+                        TempData["mensagemSucesso"] = "Compra realizada com sucesso!.";
+                    else
+                        TempData["mensagemErro"] = "Compra não pode ser realizada!.";
+                }
+                else
+                    TempData["mensagemErro"] = "Compra não pode ser finalizada pois não existe nenhum usuário logado na sessão!.";
+
+                return RedirectToAction(nameof(Index));
             }
             return View();
         }
 
         /// <summary>
-        /// Metodo adiciona valores de creditos para comprar(esses dados devem vir do banco)
+        /// Metodo que adiciona valores de creditos para comprar(esses dados devem vir do banco)
         /// </summary>
         public List<CreditoViagemViewModel> addListaCreditos()
         {
