54ae80cb24ed1164fecd1a840d765b27ef4cd0eb
Abraao Alves <abraao1514@gmail.com>
2019-08-14 23:57:31 -0300

    init branch

---
+++ Codigo/QueroTransporteSolucao/Model/ViewModel/ViagemPassagemViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model.ViewModel
+{
+    public class ViagemPassagemViewModel
+    {
+        [Key]
+        [Required]
+        public int IdViagemPassagem { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Origem { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Destino { get; set; }
+        [Required]
+        public double Preco { get; set; }
+        public bool EhCredito { get; set; }
+    }
+}
---
+++ Codigo/QueroTransporteSolucao/QueroTransporteWeb/Controllers/PagarPassagemController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QueroTransporte.Model;
+using Model.ViewModel;
+namespace QueroTransporteWeb.Controllers
+{
+    public class PagarPassagemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index(ViagemPassagemViewModel passagemView)
+        {
+            return View();
+        }
+    }
+}
