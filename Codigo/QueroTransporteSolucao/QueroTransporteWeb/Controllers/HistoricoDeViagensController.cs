cdf6ef876275982c7290defdc5949d5b8e288de4
João Alves <joaoalves.cs@gmail.com>
2019-09-09 15:48:02 -0300

    Caso de uso Consultar Histórico de Viagens.

---
+++ Codigo/QueroTransporteSolucao/QueroTransporteWeb/Controllers/HistoricoDeViagensController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business;
+using Microsoft.AspNetCore.Mvc;
+using QueroTransporte.Model;
+using QueroTransporte.Negocio;
+
+namespace QueroTransporteWeb.Controllers
+{
+
+    public class HistoricoDeViagensController : Controller
+    {
+
+        private readonly GerenciadorViagem _gerenciadorViagem;
+
+        public HistoricoDeViagensController(GerenciadorViagem gerenciadorViagem)
+        {
+            _gerenciadorViagem = gerenciadorViagem;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_gerenciadorViagem.ObterTodos());
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViagemModel viagem = _gerenciadorViagem.ObterPorId(id);
+            return View(viagem);
+        }
+    }
+}
