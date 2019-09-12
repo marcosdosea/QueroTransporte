029f5a3236360d440259e270dab65f0df56007fc
Abraao Alves <abraao1514@gmail.com>
2019-09-11 21:53:29 -0300

    Melhorando a visualização  dos relátorio para o usuário

---
+++ Codigo/QueroTransporteSolucao/Model/ViewModel/RelatorioConsumivelVeicularViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Model.ViewModel
+{
+    public class RelatorioConsumivelVeicularViewModel
+    {
+        public double Valor { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Data { get; set; }
+        public String ValorMasked{ get; set; }
+    }
+}
