a5171caafcb27ec7c22039eaed59bc8fcd3d9250
igorb22 <igorb22@live.com>
2020-01-23 21:02:50 -0300

    Adcionado projetos de teste Fix #88

---
+++ Codigo/QueroTransporteSolucao/TestService/VeiculoControllerUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestService
+{
+    public class VeiculoControllerUnitTest
+    {
+        [Fact]
+        public void Inserir()
+        {
+
+        }
+    }
+}
---
+++ Codigo/QueroTransporteSolucao/TesteController/GerenciadorVeiculoUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TesteController
+{
+    public class GerenciadorVeiculoUnitTest
+    {
+        [Fact]
+        public void GetAll()
+        {
+
+        }
+    }
+}
